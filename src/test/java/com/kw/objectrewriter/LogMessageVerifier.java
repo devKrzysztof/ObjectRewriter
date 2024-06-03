02016d0019f52a6fdf1e03572fb0529ea83993bb
KW <kw@example.com>
2024-06-04 00:59:30 +0200

    Add assertion for error level messages

--- src/test/java/com/kw/objectrewriter/LogMessageVerifier.java
+++ src/test/java/com/kw/objectrewriter/LogMessageVerifier.java
@@ -1,9 +1,12 @@
 package com.kw.objectrewriter;
 
+import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.read.ListAppender;
 import org.slf4j.LoggerFactory;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class LogMessageVerifier implements AutoCloseable {
 
@@ -16,6 +19,17 @@ public class LogMessageVerifier implements AutoCloseable {
         appender = new ListAppender<>();
         appender.start();
         logger.addAppender(appender);
+    }
+
+    public void assertErrorMessageInLogs(String message) {
+        hasMessageWithLevel(message, Level.ERROR);
+    }
+
+    private void hasMessageWithLevel(String message, Level level) {
+        assertThat(appender.list)
+                .filteredOn(c -> c.getLevel().equals(level))
+                .map(ILoggingEvent::getFormattedMessage)
+                .contains(message);
     }
 
     @Override
