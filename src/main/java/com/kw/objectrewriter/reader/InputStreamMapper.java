37475422701f256509666834b00c6ddbee21d66c
KW <kw@example.com>
2024-06-04 01:00:40 +0200

    Log error when there is an exception during mapping process

--- src/main/java/com/kw/objectrewriter/reader/InputStreamMapper.java
+++ src/main/java/com/kw/objectrewriter/reader/InputStreamMapper.java
@@ -1,12 +1,18 @@
 package com.kw.objectrewriter.reader;
 
+import com.fasterxml.jackson.core.exc.StreamReadException;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Optional;
 
 class InputStreamMapper<T> {
+    private static final Logger logger = LoggerFactory.getLogger(InputStreamMapper.class);
     private final ObjectMapper objectMapper;
     private final TypeReference<T> typeReference;
 
@@ -18,8 +24,14 @@ class InputStreamMapper<T> {
     Optional<T> mapToObject(InputStream inputStream) {
         try {
             return Optional.of(objectMapper.readValue(inputStream, typeReference));
-        } catch (Exception e) {
-            return Optional.empty();
+        } catch (StreamReadException e) {
+            logger.error("There is a problem with reading object from InputStream of type {} due to following error: {}",
+                    typeReference.getType().getTypeName(), e.getMessage(), e);
+        } catch (DatabindException e) {
+            logger.error("There was a problem with deserialization due to following error: {}", e.getMessage(), e);
+        } catch (IOException e) {
+            logger.error("There was a problem reading of object due to following error: {}", e.getMessage(), e);
         }
+        return Optional.empty();
     }
 }
--- src/test/java/com/kw/objectrewriter/reader/InputStreamMapperTest.java
+++ src/test/java/com/kw/objectrewriter/reader/InputStreamMapperTest.java
@@ -1,14 +1,23 @@
 package com.kw.objectrewriter.reader;
 
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kw.objectrewriter.LogMessageVerifier;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class InputStreamMapperTest {
 
@@ -36,4 +45,27 @@ public class InputStreamMapperTest {
         assertThat(result).hasValueSatisfying(list -> assertThat(list).contains("first", "second"));
     }
 
+    @DataProvider
+    public Object[] exceptionProvider() {
+        return new Object[][]{
+                {IOException.class, "There was a problem reading of object due to following error: null"},
+                {JsonMappingException.class, "There was a problem with deserialization due to following error: N/A"},
+                {JsonParseException.class, "There is a problem with reading object from InputStream of type java.lang.String due to following error: N/A"},
+        };
+    }
+
+    @Test(dataProvider = "exceptionProvider")
+    void shouldReturnEmptyOptionalOnException(Class<? extends Throwable> exception, String errorMessage) throws IOException {
+        ObjectMapper mockedMapper = mock(ObjectMapper.class);
+        when(mockedMapper.readValue(any(InputStream.class), any(TypeReference.class))).thenThrow(exception);
+        InputStreamMapper<String> inputStreamMapper = new InputStreamMapper<>(mockedMapper, new TypeReference<>() {
+        });
+
+        try (LogMessageVerifier verifier = new LogMessageVerifier(InputStreamMapper.class)) {
+            Optional<String> result = inputStreamMapper.mapToObject(new ByteArrayInputStream("".getBytes()));
+
+            verifier.assertErrorMessageInLogs(errorMessage);
+            assertThat(result).isEmpty();
+        }
+    }
 }
