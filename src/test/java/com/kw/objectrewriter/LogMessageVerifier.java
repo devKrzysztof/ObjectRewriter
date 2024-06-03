664b7932955f06501f88988a350d64f1e1683818
KW <kw@example.com>
2024-06-04 00:57:33 +0200

    Implement adding log appender to logger on initialization

--- src/test/java/com/kw/objectrewriter/LogMessageVerifier.java
+++ src/test/java/com/kw/objectrewriter/LogMessageVerifier.java
@@ -1,4 +1,25 @@
 package com.kw.objectrewriter;
 
-public class LogMessageVerifier {
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
+import org.slf4j.LoggerFactory;
+
+public class LogMessageVerifier implements AutoCloseable {
+
+    private final Logger logger;
+    private final ListAppender<ILoggingEvent> appender;
+
+
+    public LogMessageVerifier(Class<?> clazz) {
+        logger = (Logger) LoggerFactory.getLogger(clazz);
+        appender = new ListAppender<>();
+        appender.start();
+        logger.addAppender(appender);
+    }
+
+    @Override
+    public void close() {
+        logger.detachAppender(appender);
+    }
 }
