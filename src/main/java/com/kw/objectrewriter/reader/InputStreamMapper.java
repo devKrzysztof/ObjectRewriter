dcb6483030f9a0d390d7c910be47d118a8f8d20e
KW <kw@example.com>
2024-06-04 00:41:52 +0200

    Create InputStreamMapper class

---
+++ src/main/java/com/kw/objectrewriter/reader/InputStreamMapper.java
@@ -0,0 +1,10 @@
+package com.kw.objectrewriter.reader;
+
+import java.io.InputStream;
+import java.util.Optional;
+
+class InputStreamMapper<T> {
+    Optional<T> mapToObject(InputStream inputStream) {
+        return Optional.empty();
+    }
+}
