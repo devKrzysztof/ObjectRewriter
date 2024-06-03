23f0e15f0964809a261c76284588c931df288cd7
KW <kw@example.com>
2024-06-04 01:46:18 +0200

    Create inner Handler class

--- src/main/java/com/kw/objectrewriter/util/FileHandler.java
+++ src/main/java/com/kw/objectrewriter/util/FileHandler.java
@@ -1,6 +1,31 @@
 package com.kw.objectrewriter.util;
 
+import java.nio.file.Path;
+import java.util.Objects;
+
 public class FileHandler {
     private FileHandler() {
+    }
+
+
+    private abstract static class Handler {
+        private Handler next;
+
+        abstract void handle(Path dir);
+
+        private static Handler setHandlers(Handler first, Handler... handlers) {
+            Handler head = first;
+            for (Handler handler : handlers) {
+                head.next = handler;
+                head = handler;
+            }
+            return first;
+        }
+
+        void handleNext(Path dir) {
+            if (Objects.nonNull(next)) {
+                next.handle(dir);
+            }
+        }
     }
 }
