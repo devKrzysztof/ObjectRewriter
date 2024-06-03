cd33073b99368f16e5413d2c118e6fcf5d438c1f
KW <kw@example.com>
2024-06-04 01:48:32 +0200

    Create handler responsible for checking of permission to given directory

--- src/main/java/com/kw/objectrewriter/util/FileHandler.java
+++ src/main/java/com/kw/objectrewriter/util/FileHandler.java
@@ -1,12 +1,30 @@
 package com.kw.objectrewriter.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Objects;
 
 public class FileHandler {
+    private static final Logger logger = LoggerFactory.getLogger(FileHandler.class);
+
     private FileHandler() {
     }
 
+
+    private static class PathPermissionHandler extends Handler {
+
+        @Override
+        void handle(Path dir) {
+            if (!Files.isWritable(dir)) {
+                logger.error("Path '{}' is not writable", dir.toAbsolutePath());
+                throw new FileIOException("Path '" + dir.toAbsolutePath() + "' is not writable");
+            }
+            handleNext(dir);
+        }
+    }
 
     private abstract static class Handler {
         private Handler next;
